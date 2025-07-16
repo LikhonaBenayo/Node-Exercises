5c8d9ec6a223e84bf61db6d12ef445a4ef36801d
LikhonaBenayo <likhonabenayo@gmail.com>
2025-07-16 15:53:57 +0200

    Add files via upload

---
+++ db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',          
+  password: 'Goodman8*', // Use your MySQL password    
+  database: 'shopleft_database'
+});
+
+conn.connect(err => {
+  if (err) {
+    console.error('Connection failed:', err.message);
+    return;
+  }
+  console.log('Connected to MySQL database ✅');
+});
+
+module.exports = conn;
